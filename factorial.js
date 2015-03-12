7b650401743408a7ce46d3fc81ba79f8ddab04c6
SametSisartenep <krashbuffer@gmail.com>
2015-03-12 02:37:27 +0100

    Factorial utility added.

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function factorial ( number ) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+module.exports = function ( number ) {
+  return factorial(number);
+}
